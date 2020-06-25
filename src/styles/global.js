1f611295726191b5c369d64d57f6a91df645cbf1
Junior <jjalves96@gmail.com>
2020-06-25 08:31:05 -0300

    add global styles

---
+++ src/styles/global.js
@@ -0,0 +1,32 @@
+import { createGlobalStyle } from 'styled-components';
+
+/* 
+  #333333
+  #D02022
+  #FFFFFF
+  #CC181E
+  #878787
+*/
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+  html, body, #root {
+    height: 100%;
+  }
+  
+  body {
+    background: #fff;
+    color: #1F2C73;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-size: 16px;
+    line-height: 1.7;
+    -webkit-font-smoothing: antialiased !important;
+  }
+`;
+
+export default GlobalStyle;
