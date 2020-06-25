7046ad76d8084891d4dd86495f90b8c7ee17f707
Junior <jjalves96@gmail.com>
2020-06-25 08:25:48 -0300

    start project

---
+++ src/components/Header/styled.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const HeaderWrapper = styled.section`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const HeaderTest = styled.div``;
